817200ac061480d6097addb4ad1e76bacc0ac598
amrpupg2222-png <amrpupg2222@gmail.com>
2026-01-19 08:35:51 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 # Telegram Credentials
 # ======================
 
-API_ID = 00000000              # ← حط TG_API_ID هنا (رقم فقط)
-API_HASH = "PUT_API_HASH"      # ← حط TG_API_HASH هنا
-SESSION_STRING = "PUT_SESSION_STRING"  # ← حط SESSION هنا
-BOT_TOKEN = "PUT_BOT_TOKEN"    # ← حط توكن البوت هنا
+API_ID = 26286019              # ← حط TG_API_ID هنا (رقم فقط)
+API_HASH = "a5ed6865e86a2e3fe66100d2b6aa025b"      # ← حط TG_API_HASH هنا
+SESSION_STRING = "1BJWap1wBuxOeprmQ5cQRLWZQJk7UtbU3nUZEhblv7oaalHM0ihFCY5fsjAunoDp7vtEWOzSR-R5jv2O6mXfOtoF3ridWc4jFqwAgLNusp41sZXnoJUADtkDOo07BZ37hRg3nCQo5aJo4ke_mvfpNL4sNtpu63lzEwJD3AH2XFlauvj3dBw6-JQC2pXss-B_U8-5R1g-o2Qzv_6S5XLgtZYte9icE682spnuNboGNhgGmHPk8yA9zrqOUf8xOmeMiue7b6WX1pDUxHHz2GOvtYPsQU5iPxYS1TJbm-jRetGx63b90n713RTWBZtQfrw5vnmncNhjfVV3B4k53YWdzLJVJ-4LDm78="  # ← حط SESSION هنا
+BOT_TOKEN = "8474741810:AAEEUc9_3UiL74gFBNLzyCQGy-JL2fMaGGQ"    # ← حط توكن البوت هنا
 
 
 # ======================
