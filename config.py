47b050d223d6afc92f169259f94e6fe934b6abf4
amrpupg2222-png <amrpupg2222@gmail.com>
2026-01-19 08:26:24 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+# ======================
+# Telegram Credentials
+# ======================
+
+API_ID = 00000000              # ← حط TG_API_ID هنا (رقم فقط)
+API_HASH = "PUT_API_HASH"      # ← حط TG_API_HASH هنا
+SESSION_STRING = "PUT_SESSION_STRING"  # ← حط SESSION هنا
+BOT_TOKEN = "PUT_BOT_TOKEN"    # ← حط توكن البوت هنا
+
+
+# ======================
+# Channels
+# ======================
+
+SOURCE_CHANNEL = (
+    "ResellGifts1",
+    "recalegift",
+    "Sandoooo_124",
+    "yyong2504"
+)
+
+TARGET_CHANNEL = "@xxx_gifts_1"
